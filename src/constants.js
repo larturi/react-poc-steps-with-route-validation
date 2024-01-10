c14ec486d949ebfad5e8d7d1afa73a021333817a
Leandro Arturi <lea.arturi@gmail.com>
2024-01-10 08:06:32 -0300

    Page 404 default

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+import Page404 from "./pages/404";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
@@ -8,4 +9,5 @@ export const STEPS = [
     { id: 2, slug: '/config', component: Page2 },
     { id: 3, slug: '/game', component: Page3 },
     { id: 4, slug: '/end', component: Page4 },
+    { id: 3, slug: '*', component: Page404 },
 ];
---
+++ src/pages/404.jsx
@@ -0,0 +1,5 @@
+import PageTemplate from '../components/PageTemplate'
+
+export default function Page404() {
+  return <PageTemplate title='404' pageId={5} />
+}
